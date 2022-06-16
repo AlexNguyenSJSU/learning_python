2c892ae82c147931f765f972d0ac1d3827cc0f8a
Anh Nguyen <alexnguyen091203@gmail.com>
2022-06-15 22:13:13 -0700

    the experiment with using Lock in multithreading

--- Multithreading_learning.py
+++ Multithreading_learning.py
@@ -1,25 +1,32 @@
-#import threading
+import threading
 import multiprocessing
 import time
 
 
-def cal_square(numbers):
+def cal_square(numbers, lock):
 	print("calculate square number")
 	for n in numbers:
+		lock.acquire()
+		time.sleep(0.5)
 		print ('square:', n*n)
+		lock.release()
 
 
-def cal_cube(numbers):
+def cal_cube(numbers, lock):
 	print("calculate cube number \n")
 	for n in numbers:
+		lock.acquire()
+		time.sleep(0.5)
 		print ('cube:', n*n*n)
+		lock.release()
 
 arr = [2,3,7,9]
 
 try:
+	lock = threading.Lock()
 	t = time.time()
-	t1 = multiprocessing.Process(target=cal_square, args=(arr,))
-	t2 = multiprocessing.Process(target=cal_cube, args=(arr,))
+	t1 = threading.Thread(target=cal_square, args=(arr,lock, ))
+	t2 = threading.Thread(target=cal_cube, args=(arr,lock, ))
 	t1.start()
 	t2.start()
 	t1.join()
