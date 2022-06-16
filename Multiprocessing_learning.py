facf04358033305b943c44aebcf96d1cbf0c44a3
Anh Nguyen <alexnguyen091203@gmail.com>
2022-06-15 19:55:02 -0700

    It's a experiment with multiprocessing in Python with observing with Task Manager

---
+++ Multiprocessing_learning.py
@@ -0,0 +1,33 @@
+import threading
+import multiprocessing
+import time
+
+
+def cal_square(numbers):
+	print("calculate square number")
+	for n in numbers:
+		time.sleep(5)
+		print ('square:', n*n)
+
+
+def cal_cube(numbers):
+	print("calculate cube number \n")
+	for n in numbers:
+		time.sleep(5)
+		print ('cube:', n*n*n)
+
+arr = [2,3,7,9]
+
+try:
+	t = time.time()
+	t1 = multiprocessing.Process(target=cal_square, args=(arr,))
+	t2 = multiprocessing.Process(target=cal_cube, args=(arr,))
+	t1.start()
+	t2.start()
+	t1.join()
+	t2.join()
+	print ("done in ", time.time()- t)
+except:
+	print ("error")
+
+print("Số lượng cpu : ", multiprocessing.cpu_count())
